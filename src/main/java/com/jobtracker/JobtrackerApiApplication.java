48f58d6a932bd9b4833f4754642a7da23a6d5101
Yoana Naydenova <y.naydenova@gmail.com>
2025-07-31 15:05:26 +0300

    add project files

---
+++ src/main/java/com/jobtracker/JobtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrackerApiApplication.class, args);
+	}
+
+}
