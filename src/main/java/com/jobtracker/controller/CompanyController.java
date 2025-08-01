db71980ef3e28acfdf71e377b5f670d7481ea5ce
Yoana Naydenova <y.naydenova@gmail.com>
2025-08-01 11:18:26 +0300

    add CompanyController

---
+++ src/main/java/com/jobtracker/controller/CompanyController.java
@@ -0,0 +1,25 @@
+package com.jobtracker.controller;
+
+import com.jobtracker.model.Company;
+import com.jobtracker.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/companies")
+@RequiredArgsConstructor
+public class CompanyController {
+    private final CompanyRepository companyRepository;
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @PostMapping
+    public Company createCompany(@RequestBody Company company) {
+        return companyRepository.save(company);
+    }
+}
