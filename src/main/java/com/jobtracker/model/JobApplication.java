9f3365c088a7f2f205bcad060115d0c480f8ee66
Yoana Naydenova <y.naydenova@gmail.com>
2025-08-01 10:32:02 +0300

    add JobApplication entity

--- src/main/java/com/jobtracker/model/JobApplication.java
+++ src/main/java/com/jobtracker/model/JobApplication.java
@@ -1,4 +1,28 @@
 package com.jobtracker.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class JobApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String position;
+    private LocalDate applicationDate = LocalDate.now();
+    private String status; // "APPLIED", "INTERVIEW", "OFFER", "REJECTED"
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
 }
